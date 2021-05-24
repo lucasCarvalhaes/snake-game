db588da8d60d423a99c7fdd61fd44d3c03f6d6f2
lucasCarvalhaes <lucascarber@gmail.com>
2021-05-24 09:01:52 -0300

    create snake

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 let canvas = document.getElementById('snake')
 let context = canvas.getContext('2d')
 let box = 32
+let snake = []
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
-function criarBG() {
-    context.fillStyle = "lightgreen";
+function createBG() {
+    context.fillStyle = "#505050";
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
-criarBG()
+function createSnake() {
+    for (const pos in snake) {
+        context.fillStyle = '#50ffff'
+        context.fillRect(snake[pos].x, snake[pos].y, box, box)
+    }
+
+}
+
+createBG()
+createSnake()
