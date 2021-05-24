5e82dac456a5c2273ee50a7a1c41a2eec8217ed5
lucasCarvalhaes <lucascarber@gmail.com>
2021-05-24 10:05:49 -0300

    added food to the canvas

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 let direction = 'right'
 
 function createBG() {
@@ -21,9 +25,14 @@ function createSnake() {
 
 }
 
-document.addEventListener('keydown', update)
+function createFood() {
+    context.fillStyle = '#ff5050'
+    context.fillRect(food.x, food.y, box, box)
+}
 
-function update(event) {
+document.addEventListener('keydown', updateDirection)
+
+function updateDirection(event) {
     if (event.keyCode == 37 && direction != 'right') direction = 'left'
     if (event.keyCode == 38 && direction != 'up') direction = 'down'
     if (event.keyCode == 39 && direction != 'left') direction = 'right'
@@ -31,13 +40,14 @@ function update(event) {
 }
 
 function starGame() {
-    if (snake[0].x > 16 * box && direction == "right") snake[0].x = 0
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box
-    if (snake[0].y > 16 * box && direction == "up") snake[0].y = 0
+    if (snake[0].y > 15 * box && direction == "up") snake[0].y = 0
     if (snake[0].y < 0 && direction == "down") snake[0].y = 16 * box
 
     createBG()
     createSnake()
+    createFood()
     let snakeX = snake[0].x
     let snakeY = snake[0].y
 
@@ -55,4 +65,4 @@ function starGame() {
     snake.unshift(newHead)
 }
 
-let jogo = setInterval(starGame, 500)
+let jogo = setInterval(starGame, 200)
